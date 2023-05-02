6a75863672c485897de0830fb6e3b1a2358adde6
Michael JeanBaptiste <michaeljean3456@gmail.com>
2023-05-02 11:16:13 -0400

    changes

---
+++ Functions/copy.js
@@ -0,0 +1,7 @@
+function addNumbers(someNumber1, someNumber2){
+    console.log(someNumber);
+    let someNumber = ();
+    let someNumber1 = 10;
+    let someNumber2 = 34;
+}
+addNumbers(someNumber1, someNumber2);
---
+++ Functions/simple_functions.js
@@ -0,0 +1,21 @@
+"use strict"
+function favoriteThings(){
+
+    console.log("Michael JeanBaptiste");
+    console.log("John Wick");
+    console.log("Charlie Puth");
+}
+favoriteThings();
+
+function whyImHere(){
+
+    console.log("I joined the program for curiosity to see how it would be if I tried it and to start a new career.");
+}
+whyImHere();
+
+
+function favoritePlace(){
+
+    console.log("My favorite place to be is home, I feel the most comfortable there.");
+}
+favoritePlace();
